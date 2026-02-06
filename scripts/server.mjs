6772efbaec11a447b424351b322c43c0af1a5f7c
Steve 'Cutter' Blades <no.junk@cutterscrossing.com>
2026-02-06 10:07:16 -0500

    feat: Build updates (#29)
    BREAKING CHANGE: Updated all dependencies and retool for true ESM package.

--- scripts/server.mjs
+++ scripts/server.mjs
@@ -1,7 +1,11 @@
-const connect = require('connect');
-const path = require('path');
-const serveStatic = require('serve-static');
-const open = require('open');
+import connect from 'connect';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import serveStatic from 'serve-static';
+import open from 'open';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const port = 8081;
 
@@ -15,7 +19,7 @@ connect()
     serveStatic(path.join(__dirname, '../dist'), {
       index: false,
       setHeaders: setHeaders,
-    })
+    }),
   )
   .use(serveStatic(path.join(__dirname, '../public')))
   .listen(port, function () {
